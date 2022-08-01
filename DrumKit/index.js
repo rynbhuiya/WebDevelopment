fc6089b73420b777693b5983cfc0f84370b4a338
rynbhuiya <rynbhuiya@gmail.com>
2022-08-01 14:07:31 -0500

    Added background images for buttons

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -1,5 +1,5 @@
-document.querySelector("button").addEventListener("click", handleClick);
-
-function handleClick() {
-  alert("I got clicked");
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    this.style.color = "white";
+  });
 }
