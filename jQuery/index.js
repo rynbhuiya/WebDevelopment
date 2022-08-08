df11ae54b8f53a004f57c8c1d61736490ff52280
rynbhuiya <rynbhuiya@gmail.com>
2022-08-08 18:07:41 -0500

    Starting Simon Game website

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,2 +1,27 @@
+$("h1").addClass("big-title");
 
-$("h1").css("color", "red");
+// Add click event listener to h1
+$("h1").click(function () {
+  $("h1").css("color", "purple");
+});
+
+// Add click event listener to all buttons
+$("button").click(function() {
+  $("h1").css("color", "purple");
+})
+
+// Add keypress event listener
+$("input").keypress(function(e) {
+  console.log(e.key);
+})
+
+// Add click event listener to document
+$(document).keypress(function(e) {
+  $("h1").text(e.key);
+})
+
+
+// More flexibile way for event listener
+$("h1").on("mouseover", function() {
+  $("h1").css("color", "green");
+})
