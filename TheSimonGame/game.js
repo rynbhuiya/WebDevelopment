ef3151de593ee6279c20fbe2a939e1f9679efd61
rynbhuiya <rynbhuiya@gmail.com>
2022-08-09 17:51:43 -0500

    Added animation/sound + click detection to buttons

---
+++ TheSimonGame/game.js
@@ -0,0 +1,28 @@
+buttonColors = ["red", "blue", "green", "yellow"];
+gamePattern = [];
+userClickedPattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  return randomNumber;
+}
+
+var randomChosenColor = buttonColors[nextSequence()];
+gamePattern.push(randomChosenColor);
+
+// Flash random button with sound
+console.log(randomChosenColor)
+$("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+
+var audio = new Audio('./sounds/' + randomChosenColor + '.mp3');
+audio.play();
+
+// Find which button was pressed
+var userChosenColor;
+$(".btn").on("click", function() {
+  userChosenColor = this.id;
+  console.log("user chosen color: " + userChosenColor);
+
+  userClickedPattern.push(userChosenColor);
+  console.log(userClickedPattern);
+})
