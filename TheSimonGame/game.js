9f5a8a9d154566257c37b8dd559aa69abfba8cc6
rynbhuiya <rynbhuiya@gmail.com>
2022-08-10 13:28:13 -0500

    Start game and check answer functions created

--- TheSimonGame/game.js
+++ TheSimonGame/game.js
@@ -1,27 +1,31 @@
-buttonColors = ["red", "blue", "green", "yellow"];
-gamePattern = [];
-userClickedPattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
 
 function nextSequence() {
+  // Change h1 text to current level
+  level++;
+  $("#level-title").text("Level " + level);
+
+  userClickedPattern = [];
+  // Find random color
   var randomNumber = Math.floor(Math.random() * 4);
-  return randomNumber;
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  // Flash random button with sound
+  console.log("gamePattern: ", gamePattern);
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio('./sounds/' + randomChosenColor + '.mp3');
+  audio.play();
 }
-
-var randomChosenColor = buttonColors[nextSequence()];
-gamePattern.push(randomChosenColor);
-
-// Flash random button with sound
-console.log(randomChosenColor)
-$("#" + randomChosenColor).fadeOut(100).fadeIn(100);
-
-var audio = new Audio('./sounds/' + randomChosenColor + '.mp3');
-audio.play();
 
 // Find which button was pressed
 var userChosenColor;
 $(".btn").on("click", function() {
   userChosenColor = this.id;
-  console.log("user chosen color: " + userChosenColor);
 
   // Play audio for clicked button
   playSound(userChosenColor);
@@ -30,8 +34,10 @@ $(".btn").on("click", function() {
   animatePress(userChosenColor);
 
   userClickedPattern.push(userChosenColor);
-  console.log(userClickedPattern);
-})
+  console.log("userClickedPattern: ", userClickedPattern);
+
+  checkAnswer(userClickedPattern.length);
+});
 
 
 function playSound(name) {
@@ -44,4 +50,24 @@ function animatePress(currentColor) {
   setTimeout(function() {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+// Detect start of game
+$(document).on("keypress", function(e) {
+  if (level === 0) {
+    nextSequence();
+  }
+});
+
+// Check if answer is correct
+function checkAnswer(idx) {
+  if (gamePattern[idx-1] === userClickedPattern[idx-1]) {
+    console.log("success");
+    if (level === idx) {
+      setTimeout(nextSequence, 1000);
+    }
+  }
+  else {
+    console.log("wrong");
+  }
 }
