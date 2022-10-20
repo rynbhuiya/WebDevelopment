26cbc250ab6ec6e0b0abe0c4265ee57c71c2a61f
rynbhuiya <rynbhuiya@gmail.com>
2022-10-20 14:48:29 -0500

    Keeper app can now add/delete notes

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../notes';
+import CreateArea from './CreateArea';
 
 function App() {
+    const [notes, setNotes] = useState([]);
+    function addNote(note) {
+        setNotes([...notes, note]);
+    }
+
+    function deleteNote(id) {
+        setNotes(curr => {
+            return curr.filter((note, idx) => (
+                idx !== id
+            ))
+        })
+    }
+
     return (
         <div> 
             <Header />
-            {notes.map(entry => (
-                <Note
-                    key={entry.key}
-                    title={entry.title}
-                    content={entry.content}
+            <CreateArea addNote={addNote}/>
+            {notes.map((note, idx) => (
+                <Note 
+                    id={idx}
+                    title={note.title}
+                    content={note.content}
+                    deleteNote={deleteNote}
                 />
             ))}
             <Footer />
---
+++ keeper-app/src/components/CreateArea.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+
+        setNote(curr => {
+            return {
+                ...curr,
+                [name]: value
+            }
+        })
+    }
+    
+    function handleSubmit(event) {
+        props.addNote(note);
+        setNote({
+            title: "",
+            content: ""
+        })
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input name="title" placeholder="Title" onChange={handleChange} value={note.title}/>
+                <textarea name="content" placeholder="Take a note..." rows="3" onChange={handleChange} value={note.content}/>
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    );
+}
+
+export default CreateArea;
